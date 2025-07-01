67a51e951c5c392a30217bcd3bb2ab601785f7ed
StdAfx <stdafx86@gmail.com>
2025-07-01 17:51:01 +0300

    = d

--- config/config.exs
+++ config/config.exs
@@ -2,11 +2,12 @@ import Config
 
 config :http_proxy,
   https: false,
-  proxies: [    
-    %{port: 4001, to: "https://api1.binance.com/"},
-    %{port: 4002, to: "https://api2.binance.com/"},
-    %{port: 4003, to: "https://api3.binance.com/"},
-    %{port: 4004, to: "https://fapi.binance.com/"},
-    %{port: 4005, to: "https://dapi.binance.com/"}
+  proxies: [
+    %{port: 5000, to: "https://api.binance.com/"},
+    %{port: 5001, to: "https://api1.binance.com/"},
+    %{port: 5002, to: "https://api2.binance.com/"},
+    %{port: 5003, to: "https://api3.binance.com/"},
+    %{port: 5004, to: "https://fapi.binance.com/"},
+    %{port: 5005, to: "https://dapi.binance.com/"}
   ],
   timeout: 30_000

83af0d34d1e82dedc92de44946d740c0d5e8e868
StdAfx <stdafx86@gmail.com>
2025-07-02 11:54:33 +0300

    = d

--- config/config.exs
+++ config/config.exs
@@ -3,11 +3,6 @@ import Config
 config :http_proxy,
   http: [port: String.to_integer(System.get_env("PORT") || "10000")],
   proxies: [
-    %{port: 5000, to: "https://api.binance.com/"},
-    %{port: 5001, to: "https://api1.binance.com/"},
-    %{port: 5002, to: "https://api2.binance.com/"},
-    %{port: 5003, to: "https://api3.binance.com/"},
-    %{port: 5004, to: "https://fapi.binance.com/"},
-    %{port: 5005, to: "https://dapi.binance.com/"}
+    %{port: 10000, to: "https://api.binance.com"}
   ],
   timeout: 30_000

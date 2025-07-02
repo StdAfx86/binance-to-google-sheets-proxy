6ffefd0d822c881db7da1114075198f96b3f4e8c
StdAfx <stdafx86@gmail.com>
2025-07-02 11:25:42 +0300

    = d

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,7 @@
 import Config
 
 config :http_proxy,
-  http: [port: 80], # необязательно, если нужен только HTTPS
-  https: [
-    port: 443,
-    cipher_suite: :strong,
-    keyfile: "priv/ssl/privkey.pem",
-    certfile: "priv/ssl/fullchain.pem"
-  ],
+  http: [port: String.to_integer(System.get_env("PORT") || "10000")],
   proxies: [
     %{port: 5000, to: "https://api.binance.com/"},
     %{port: 5001, to: "https://api1.binance.com/"},
